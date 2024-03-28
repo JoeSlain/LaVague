496a5356ef6d47402ae1213dba6a12cf830fd4ce
Matthias BRUNEL <mbrunel@student.42.fr>
2024-03-28 20:09:32 +0000

    better abstraction

--- src/lavague/telemetry.py
+++ src/lavague/telemetry.py
@@ -5,9 +5,15 @@ import uuid
 TELEMETRY_VAR = os.getenv("LAVAGUE_TELEMETRY")
 USER_ID = str(uuid.uuid4())
 
-def send_telemetry(model_name, code, screenshot, html, source_nodes, instruction, url, origin):
+
+def send_telemetry(
+    model_name, code, screenshot, html, source_nodes, instruction, url, origin
+):
     try:
         if TELEMETRY_VAR is None:
-            r = requests.post('https://telemetrylavague.mithrilsecurity.io/telemetry', json={"llm": model_name, "user_id": USER_ID, "origin": origin})
+            r = requests.post(
+                "https://telemetrylavague.mithrilsecurity.io/telemetry",
+                json={"llm": model_name, "user_id": USER_ID, "origin": origin},
+            )
     except Exception as e:
         pass
